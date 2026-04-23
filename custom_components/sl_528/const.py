ca7ab4515741a94a944df30ec4b25b4d7763efcf
Tokmos <166540386+Tokmos@users.noreply.github.com>
2026-04-23 16:16:34 +0200

    Delete custom_components/sl_528 directory

--- custom_components/sl_528/const.py
+++
@@ -1,6 +0,0 @@
-DOMAIN = "sl_528"
-CONF_API_KEY = "api_key"
-CONF_LINE = "line"
-DEFAULT_LINE = "528"
-SCAN_INTERVAL_SECONDS = 15
-GTFS_RT_URL = "https://opendata.samtrafiken.se/gtfs-rt/sl/VehiclePositions.pb?key={api_key}"
